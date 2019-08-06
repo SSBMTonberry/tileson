d2dc028377d20c2de5600fb2670016452d7f9130
Robin Pettersen <ssbmtonberry@gmail.com>
2019-08-06 20:34:54 +0200

    Started working on parsing logic.

--- src/Tileson.cpp
+++ src/Tileson.cpp
@@ -2,4 +2,37 @@
 // Created by robin on 31.07.2019.
 //
 
+
 #include "Tileson.h"
+
+tson::Map tson::Tileson::parse(const fs::path &path)
+{
+    if(fs::exists(path) && fs::is_regular_file(path))
+    {
+        std::ifstream i(path.u8string());
+        nlohmann::json json;
+        i >> json;
+        return parseJson(json);
+    }
+}
+
+tson::Map tson::Tileson::parse(const void *data, size_t size)
+{
+    std::istringstream i;
+    i.rdbuf()->pubsetbuf((char *)data, size);
+    nlohmann::json json;
+    i >> json;
+
+    return parseJson(json);
+}
+
+tson::Map tson::Tileson::parseJson(const nlohmann::json &json)
+{
+    tson::Map map;
+    if(map.parse(json))
+        return map;
+
+    return tson::Map {};
+}
+
+
--- src/Tileson.h
+++ src/Tileson.h
@@ -5,12 +5,45 @@
 #ifndef TILESON_TILESON_H
 #define TILESON_TILESON_H
 
+#include "tiled/Map.h"
+#include <fstream>
+
+#if MSVC
+#include <filesystem>
+    namespace fs = std::filesystem;
+#elif MINGW
+#if __MINGW64_VERSION_MAJOR > 6
+        #include <filesystem>
+        namespace fs = std::filesystem;
+    #else
+        #include <experimental/filesystem>
+        namespace fs = std::experimental::filesystem;
+    #endif
+#elif APPLE
+#include <experimental/filesystem>
+    namespace fs = std::experimental::filesystem;
+#else
+    #if __GNUC__ < 8 //GCC major version less than 8
+#include <experimental/filesystem>
+        namespace fs = std::experimental::filesystem;
+    #else
+        #include <filesystem>
+namespace fs = std::filesystem;
+    #endif
+#endif
+
 namespace tson
 {
     class Tileson
     {
         public:
             Tileson() = default;
+
+            tson::Map parse(const fs::path &path);
+            tson::Map parse(const void * data, size_t size);
+
+        private:
+            tson::Map parseJson(const nlohmann::json &json);
     };
 }
 
