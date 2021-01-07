4032c20912f8bdb835ffe757046c1f656bf68b9d
Robin Pettersen <ssbmtonberry@gmail.com>
2021-01-07 07:50:29 +0100

    IJson has a fully working get<T>() - proven with fake implementation #23

--- include/interfaces/IJson.hpp
+++ include/interfaces/IJson.hpp
@@ -16,9 +16,41 @@ namespace tson
             [[nodiscard]] T get(std::string_view key) const;
             [[nodiscard]] virtual size_t count(std::string_view key) const = 0;
             [[nodiscard]] virtual bool any(std::string_view key) const = 0;
-            //[[nodiscard]] virtual const std::string &name() const = 0;
+            [[nodiscard]] virtual bool isArray() const = 0;
+            [[nodiscard]] virtual bool isObject() const = 0;
+            [[nodiscard]] virtual bool isNull() const = 0;
 
+        protected:
+            [[nodiscard]] virtual int32_t getInt32(std::string_view key) const = 0;
+            [[nodiscard]] virtual uint32_t getUInt32(std::string_view key) const = 0;
+            [[nodiscard]] virtual int64_t getInt64(std::string_view key) const = 0;
+            [[nodiscard]] virtual uint64_t getUInt64(std::string_view key) const = 0;
+
+            [[nodiscard]] virtual double getDouble(std::string_view key) const = 0;
+            [[nodiscard]] virtual const std::string &getString(std::string_view key) const = 0;
+            [[nodiscard]] virtual bool getBool(std::string_view key) const = 0;
     };
+
+    template<typename T>
+    T IJson::get(std::string_view key) const
+    {
+        if constexpr (std::is_same<T, double>::value)
+            return getDouble(key);
+        else if constexpr (std::is_same<T, int32_t>::value)
+            return getInt32(key);
+        else if constexpr (std::is_same<T, uint32_t>::value)
+            return getUInt32(key);
+        else if constexpr (std::is_same<T, int64_t>::value)
+            return getInt64(key);
+        else if constexpr (std::is_same<T, uint64_t>::value)
+            return getUInt64(key);
+        else if constexpr (std::is_same<T, std::string>::value)
+            return getString(key);
+        else if constexpr (std::is_same<T, bool>::value)
+            return getBool(key);
+        else
+            return nullptr;
+    }
 
 }
 
--- tests/tests_interfaces.cpp
+++ tests/tests_interfaces.cpp
@@ -16,15 +16,68 @@ class FakeSon : public tson::IJson
 
         [[nodiscard]] size_t count(std::string_view key) const override
         {
-            return 0;
+            return (key == "test") ? 1 : 0;
         }
 
         [[nodiscard]] bool any(std::string_view key) const override
         {
+            return (key == "test");
+        }
+
+        [[nodiscard]] bool isArray() const override
+        {
             return false;
         }
 
+        [[nodiscard]] bool isObject() const override
+        {
+            return true;
+        }
 
+        [[nodiscard]] bool isNull() const override
+        {
+            return false;
+        }
+
+    protected:
+        [[nodiscard]] int32_t getInt32(std::string_view key) const override
+        {
+            return (key == "test") ? -32 : -1;
+        }
+
+        [[nodiscard]] uint32_t getUInt32(std::string_view key) const override
+        {
+            return (key == "test") ? 32 : 0;
+        }
+
+        [[nodiscard]] int64_t getInt64(std::string_view key) const override
+        {
+            return (key == "test") ? -64 : -1;
+        }
+
+        [[nodiscard]] uint64_t getUInt64(std::string_view key) const override
+        {
+            return (key == "test") ? 64 : 0;
+        }
+
+        [[nodiscard]] double getDouble(std::string_view key) const override
+        {
+            return (key == "test") ? 1337.1337 : 1.0;
+        }
+
+        [[nodiscard]] const std::string &getString(std::string_view key) const override
+        {
+            return (key == "test") ? m_str : m_str2;
+        }
+
+        [[nodiscard]] bool getBool(std::string_view key) const override
+        {
+            return (key == "test");
+        }
+
+    private:
+        std::string m_str {"Test"};
+        std::string m_str2 {"No value"};
 };
 
 //template<typename T>
@@ -34,8 +87,27 @@ class FakeSon : public tson::IJson
 //    return defaultT;
 //}
 
-TEST_CASE( "Test a simple implementation of IJson - Expect OK.", "[color][argb]" ) {
+TEST_CASE( "Test a simple FAKE-implementation of IJson - Expect OK.", "[json][interface]" ) {
     std::unique_ptr<tson::IJson> j = std::make_unique<FakeSon>();
-    int i = j->get<int>("Hey, brother");
-    REQUIRE( true );
+    REQUIRE( j->count("test") == 1 );
+    REQUIRE( j->count("best") == 0 );
+    REQUIRE( j->any("test"));
+    REQUIRE( !j->any("best"));
+    REQUIRE( j->isObject());
+
+    REQUIRE( j->get<int32_t>("test") == -32 );
+    REQUIRE( j->get<int32_t>("best") == -1 );
+    REQUIRE( j->get<uint32_t>("test") == 32 );
+    REQUIRE( j->get<uint32_t>("best") == 0 );
+    REQUIRE( j->get<int64_t>("test") == -64 );
+    REQUIRE( j->get<int64_t>("best") == -1 );
+    REQUIRE( j->get<uint64_t>("test") == 64 );
+    REQUIRE( j->get<uint64_t>("best") == 0 );
+    REQUIRE( j->get<double>("test") > 1337.0 );
+    REQUIRE( j->get<double>("best") < 2 );
+    REQUIRE( j->get<std::string>("test") == "Test" );
+    REQUIRE( j->get<std::string>("best") == "No value" );
+    REQUIRE( j->get<bool>("test"));
+    REQUIRE( !j->get<bool>("best"));
+    REQUIRE( j->get<nullptr_t>("whatever") == nullptr);
 }
