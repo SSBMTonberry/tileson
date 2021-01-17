395748544467134b2c42caadf09ad54c8dddf6fc
Robin Pettersen <ssbmtonberry@gmail.com>
2021-01-17 11:13:31 +0100

    LZMA decompression using file and memory with PocketLzma now works! #14

---
+++ content/memory/files_mapper.h
@@ -0,0 +1,10 @@
+#ifndef F2SRC_ASSET_FILES_FILE_MAPPER
+#define F2SRC_ASSET_FILES_FILE_MAPPER
+#include "files.h"
+
+namespace files_mapper
+{
+	static const unsigned char * _ULTIMATE_TEST_LZMA = files::_ULTIMATE_TEST_LZMA;
+	static const size_t _ULTIMATE_TEST_LZMA_SIZE = files::_ULTIMATE_TEST_LZMA_SIZE;
+}
+#endif //F2SRC_ASSET_FILES_FILE_MAPPER
--- tests/tests_benchmarks.cpp
+++ tests/tests_benchmarks.cpp
@@ -3,10 +3,16 @@
 //
 
 #include "../external_libs/catch.hpp"
+
+//#define POCKETLZMA_LZMA_C_DEFINE //Already defined once in tests_main
+#include "../extras/pocketlzma.hpp"
+
 #include "../include/external/nlohmann.hpp"
 #include "../include/external/picojson.hpp"
 #include "../include/external/gason.hpp"
 #include "../include/tileson.h"
+
+#include "tson_files_mapper.h"
 
 //Must be defined after Tileson
 #include "../include/json/Gason.hpp"
@@ -16,6 +22,10 @@ TEST_CASE( "Run benchmarks on all supported json-backends + Gason", "[json][benc
     fs::path pathLocal {"../../content/test-maps/ultimate_test.json"};
     fs::path pathTravis {"../content/test-maps/ultimate_test.json"};
     fs::path pathToUse = (fs::exists(pathLocal)) ? pathLocal : pathTravis;
+
+    fs::path pathLzmaLocal {"../../content/test-maps/ultimate_test.lzma"};
+    fs::path pathLzmaTravis {"../content/test-maps/ultimate_test.lzma"};
+    fs::path pathLzmaToUse = (fs::exists(pathLzmaLocal)) ? pathLzmaLocal : pathLzmaTravis;
 
     tson::Tileson nlohmann {std::make_unique<tson::NlohmannJson>()};
     tson::Tileson picojson {std::make_unique<tson::PicoJson>()};
@@ -42,12 +52,27 @@ TEST_CASE( "Run benchmarks on all supported json-backends + Gason", "[json][benc
     auto endJ11 = std::chrono::steady_clock::now();
     std::chrono::duration<double> msJ11 = (endJ11-startJ11) * 1000;
 
-    std::cout << "Nlohmann parse time: " << msNlohmann.count() << " ms\n";
-    std::cout << "PicoJson parse time: " << msPicoJson.count() << " ms\n";
-    std::cout << "Gason parse time: " << msGason.count() << " ms\n";
-    std::cout << "Json11 parse time: " << msJ11.count() << " ms\n";
+    auto startJ11Lzma = std::chrono::steady_clock::now();
+    auto j11MapLzma = j11.parse(pathLzmaToUse, std::make_unique<tson::Lzma>());
+    auto endJ11Lzma = std::chrono::steady_clock::now();
+    std::chrono::duration<double> msJ11Lzma = (endJ11Lzma-startJ11Lzma) * 1000;
+
+    auto startJ11LzmaMemory = std::chrono::steady_clock::now();
+    std::unique_ptr<tson::Map> j11MapLzmaMemory = j11.parse(tson_files_mapper::_ULTIMATE_TEST_LZMA, tson_files_mapper::_ULTIMATE_TEST_LZMA_SIZE, std::make_unique<tson::Lzma>());
+    auto endJ11LzmaMemory = std::chrono::steady_clock::now();
+    std::chrono::duration<double> msJ11LzmaMemory = (endJ11LzmaMemory-startJ11LzmaMemory) * 1000;
+
+    std::cout << "Nlohmann parse time:             " << msNlohmann.count() << " ms\n";
+    std::cout << "PicoJson parse time:             " << msPicoJson.count() << " ms\n";
+    std::cout << "Gason parse time:                " << msGason.count() << " ms\n";
+    std::cout << "Json11 parse time:               " << msJ11.count() << " ms\n";
+    std::cout << "Json11 parse time (LZMA):        " << msJ11Lzma.count() << " ms\n";
+    std::cout << "Json11 memory parse time (LZMA): " << msJ11LzmaMemory.count() << " ms\n";
 
     REQUIRE(nlohmannMap->getStatus() == tson::ParseStatus::OK);
     REQUIRE(picojsonMap->getStatus() == tson::ParseStatus::OK);
     REQUIRE(gasonMap->getStatus() == tson::ParseStatus::OK);
+    REQUIRE(j11Map->getStatus() == tson::ParseStatus::OK);
+    REQUIRE(j11MapLzma->getStatus() == tson::ParseStatus::OK);
+    REQUIRE(j11MapLzmaMemory->getStatus() == tson::ParseStatus::OK);
 }
