3eadc9a384b782d8b463d7de5bba2a985f8d4792
Robin Pettersen <ssbmtonberry@gmail.com>
2021-02-06 09:47:37 +0100

    Added margin-space tileset and map for tests #31
    Also made it lzma-compressed to showcase compression in demo.

--- tools/pocketlzma_script.sh
+++ tools/pocketlzma_script.sh
@@ -1,1 +1,2 @@
 ./pocketlzma -cg ./../content/test-maps/ultimate_test.json ./../content/test-maps/ultimate_test.lzma
+./pocketlzma -cg ./../content/test-maps/margin-space-map.json ./../content/test-maps/margin-space-map.lzma
